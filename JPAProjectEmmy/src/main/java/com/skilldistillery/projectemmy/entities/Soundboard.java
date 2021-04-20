c18cc3743f2bdc3bf7e265fb3e0a1e5e62b8a76e
lisies93 <solorzanolisseth83@gmail.com>
2021-04-20 17:36:55 -0600

    Soundboard Entity created and tested

---
+++ JPAProjectEmmy/src/main/java/com/skilldistillery/projectemmy/entities/Soundboard.java
@@ -0,0 +1,114 @@
+package com.skilldistillery.projectemmy.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Soundboard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	@Column(name = "is_public")
+	private Boolean isPublic;
+	
+	private String description;
+	
+	@Column(name="create_date")
+	@CreationTimestamp
+	private LocalDateTime createDate;
+	
+	@Column(name = "is_default")
+	private Boolean isDefault;
+	
+	public Soundboard() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getIsPublic() {
+		return isPublic;
+	}
+
+	public void setIsPublic(Boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public Boolean getIsDefault() {
+		return isDefault;
+	}
+
+	public void setIsDefault(Boolean isDefault) {
+		this.isDefault = isDefault;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Soundboard other = (Soundboard) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Soundboard [id=" + id + ", name=" + name + ", isPublic=" + isPublic + ", description=" + description
+				+ ", createDate=" + createDate + ", isDefault=" + isDefault + "]";
+	}
+	
+	
+	
+}
