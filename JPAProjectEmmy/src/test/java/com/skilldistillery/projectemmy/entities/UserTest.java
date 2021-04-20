b22ff885c03f8532520aba5938e48761e7f1b409
tylorshearn <tylorshearn95@gmail.com>
2021-04-20 13:59:23 -0400

    Configured the persistence unit and completed a stub User entity test.

--- JPAProjectEmmy/src/test/java/com/skilldistillery/projectemmy/entities/UserTest.java
+++ JPAProjectEmmy/src/test/java/com/skilldistillery/projectemmy/entities/UserTest.java
@@ -1,34 +1,52 @@
 package com.skilldistillery.projectemmy.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
-
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("ProjectEmmyPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
+	@DisplayName("Stub User mapping test")
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
 	}
 
 }
