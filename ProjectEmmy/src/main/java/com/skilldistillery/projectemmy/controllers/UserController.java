fd6dc1b106f33e8389a3a9716d8c8d90d61431bb
lisies93 <solorzanolisseth83@gmail.com>
2021-04-20 13:48:36 -0600

    Stub angular project,add @CrossOrigin

--- ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/controllers/UserController.java
+++ ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/controllers/UserController.java
@@ -1,12 +1,14 @@
 package com.skilldistillery.projectemmy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.projectemmy.services.UserService;
 
+@CrossOrigin({"*", "http://localhost:4290"})
 @RequestMapping("api")
 @RestController
 public class UserController {
