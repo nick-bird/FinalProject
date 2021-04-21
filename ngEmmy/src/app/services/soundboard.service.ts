fae836fe057a7bb192d0cf040f0c850848315f2f
Nick Bird <bird.nick.95@gmail.com>
2021-04-21 13:40:47 -0600

    created auth service in ng project and imported to soundboard service

--- ngEmmy/src/app/services/soundboard.service.ts
+++ ngEmmy/src/app/services/soundboard.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoundboardService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) { }
 }
