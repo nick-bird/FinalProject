b06ca642bf146cce2296f19d27f18a4ff1cb853a
lisies93 <solorzanolisseth83@gmail.com>
2021-04-21 17:19:35 -0600

    category and image service (angular) created

--- ngEmmy/src/app/models/image.ts
+++ ngEmmy/src/app/models/image.ts
@@ -1,7 +1,7 @@
 export class Image {
   id: number;
   imageUrl: string;
-  constructor(id: number, imageUrl: string){
+  constructor(id?: number, imageUrl?: string){
     this.id = id;
     this.imageUrl = imageUrl;
   }
