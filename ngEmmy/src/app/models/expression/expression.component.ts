800da168bd5300087d937e7053db27251825c79d
Justin <>
2021-04-21 13:09:19 -0400

    Stubbed models and services

---
+++ ngEmmy/src/app/models/expression/expression.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expression',
+  templateUrl: './expression.component.html',
+  styleUrls: ['./expression.component.css']
+})
+export class ExpressionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
