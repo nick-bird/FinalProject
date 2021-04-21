4d234eba136f7d632b2cae61044ad91bbcd96e47
Justin <>
2021-04-21 13:34:39 -0400

    build soundboard Service

--- ngEmmy/src/app/models/soundboard.ts
+++ ngEmmy/src/app/models/soundboard.ts
@@ -1,2 +1,19 @@
 export class Soundboard {
+  id: number;
+  name: string;
+  description: string;
+  createDate: string;
+  isPublic: boolean;
+  isDefault: boolean;
+
+  constructor(id: number, name: string, description: string, createDate: string, isPublic: boolean, isDefault: boolean ){
+
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.createDate = createDate;
+    this.isPublic = isPublic;
+    this.isDefault = isDefault;
+  }
+
 }
