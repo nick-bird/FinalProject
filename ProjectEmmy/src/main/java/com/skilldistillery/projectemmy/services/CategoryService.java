56e26cb7da66387eaf118f5e3d10a51f14659fb3
lisies93 <solorzanolisseth83@gmail.com>
2021-04-21 16:20:36 -0600

    category service,controller,repository created

---
+++ ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/services/CategoryService.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.projectemmy.services;
+
+import java.util.List;
+
+import com.skilldistillery.projectemmy.entities.Category;
+
+public interface CategoryService {
+
+	public List<Category> index();
+	
+}
