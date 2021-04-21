f531546c67a7520af9f29f76f42df2974e9d19da
Justin <>
2021-04-21 15:54:57 -0400

    could not work on area. lines are commented out to get working. Un comment

--- ngEmmy/src/app/app-routing.module.ts
+++ ngEmmy/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', component: NotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ngEmmy/src/app/components/profile/profile.component.ts
+++ ngEmmy/src/app/components/profile/profile.component.ts
@@ -17,7 +17,7 @@ export class ProfileComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-  this.reload();
+  // this.reload();
   }
 
 soundboards: Soundboard[] = [];
@@ -29,114 +29,114 @@ newExpression: Expression = null;
 editSoundboard: Soundboard = null;
 editExpression: Expression = null;
 
-reload() {
-  this.soundboardService.index().subscribe(
-    data => {
-      this.soundboards = data;
-    },
-    err => {
-      console.log('Error loading soundboards: ' + err);
-    }
-  );
-}
-displaySoundboard(soundboard: Soundboard): void {
-  this.selectedSoundboard = soundboard;
-}
+// reload() {
+//   this.soundboardService.index().subscribe(
+//     data => {
+//       this.soundboards = data;
+//     },
+//     err => {
+//       console.log('Error loading soundboards: ' + err);
+//     }
+//   );
+// }
+// displaySoundboard(soundboard: Soundboard): void {
+//   this.selectedSoundboard = soundboard;
+// }
 
-displayExpression(expression: Expression): void {
-  this.selectedExpression = expression;
-}
+// displayExpression(expression: Expression): void {
+//   this.selectedExpression = expression;
+// }
 
-displaySoundboards(): void {
-  this.selectedSoundboard = null;
-  this.selectedExpression = null;
-}
+// displaySoundboards(): void {
+//   this.selectedSoundboard = null;
+//   this.selectedExpression = null;
+// }
 
-addSoundboard(): void {
-  console.log(this.newSoundboard);
-  this.soundboardService.create(this.newSoundboard).subscribe(
-    data => {
-      this.displaySoundboard(this.newSoundboard);
-    },
-    err => {
-      console.log('Error creating todo: ' + err)
-    }
-  );
-}
+// addSoundboard(): void {
+//   console.log(this.newSoundboard);
+//   this.soundboardService.create(this.newSoundboard).subscribe(
+//     data => {
+//       this.displaySoundboard(this.newSoundboard);
+//     },
+//     err => {
+//       console.log('Error creating todo: ' + err)
+//     }
+//   );
+// }
 
-  addExpression(): void {
-    console.log(this.newExpression);
-    this.expressionService.create(this.newExpression).subscribe(
-      data => {
-        this.displayExpression(this.newExpression);
-      },
-      err => {
-        console.log('Error creating expression: ' + err)
-      }
-    );
-  }
+//   addExpression(): void {
+//     console.log(this.newExpression);
+//     this.expressionService.create(this.newExpression).subscribe(
+//       data => {
+//         this.displayExpression(this.newExpression);
+//       },
+//       err => {
+//         console.log('Error creating expression: ' + err)
+//       }
+//     );
+//   }
 
-    setEditExpression(): void {
-      this.editExpression = Object.assign({}, this.selectedExpression);
-    }
-    setEditSoundboard(): void {
-      this.editSoundboard = Object.assign({}, this.selectedSoundboard);
-    }
-    updateSoundboard(editedSoundboard: SoundboardService, displaySoundboard = true): void {
-      this.soundboardService.update(editedSoundboard).subscribe(
-        data => {
-          this.reload();
-          if(displaySoundboard){
-            this.selectedSoundboard = editedSoundboard;
-          }
-           this.editSoundboard = null;
-        },
-        err => {
-          console.log('Error updating soundboard: ' + err);
-        }
-      );
-    }
-      updateExpression(editedExpresion: Expression, displayExpression = true): void {
-        this.expressionService.update(editedExpresion).subscribe(
-          data => {
-            this.reload();
-            if(displayExpression){
-              this.selectedExpression = editedExpression;
-            }
-             this.editExpression = null;
-          },
-          err => {
-            console.log('Error updating expression: ' + err);
-          }
-        )
-      };
+//     setEditExpression(): void {
+//       this.editExpression = Object.assign({}, this.selectedExpression);
+//     }
+//     setEditSoundboard(): void {
+//       this.editSoundboard = Object.assign({}, this.selectedSoundboard);
+//     }
+//     updateSoundboard(editedSoundboard: SoundboardService, displaySoundboard = true): void {
+//       this.soundboardService.update(editedSoundboard).subscribe(
+//         data => {
+//           this.reload();
+//           if(displaySoundboard){
+//             this.selectedSoundboard = editedSoundboard;
+//           }
+//            this.editSoundboard = null;
+//         },
+//         err => {
+//           console.log('Error updating soundboard: ' + err);
+//         }
+//       );
+//     }
+//       updateExpression(editedExpresion: Expression, displayExpression = true): void {
+//         this.expressionService.update(editedExpresion).subscribe(
+//           data => {
+//             this.reload();
+//             if(displayExpression){
+//               this.selectedExpression = editedExpression;
+//             }
+//              this.editExpression = null;
+//           },
+//           err => {
+//             console.log('Error updating expression: ' + err);
+//           }
+//         )
+//       };
 
-        deleteSoundboard(id: number): void {
-          // if (confirm('You sure about this?')) {
-            // this.todos = this.todoService.index();
-            // }
-            this.soundboardService.destroy(id).subscribe(
-              data =>{
-                this.reload();
-              },
-              err => {
-                console.log('Error deleting soundboard: ' + err);
-              }
-            );
-        }
-        deleteExpression(id: number): void {
-          // if (confirm('You sure about this?')) {
-            // this.todos = this.todoService.index();
-            // }
-            this.expressionService.destroy(id).subscribe(
-              data =>{
-                this.reload();
-              },
-              err => {
-                console.log('Error deleting expression: ' + err);
-              }
-            );
-        }
+//         deleteSoundboard(id: number): void {
+//           // if (confirm('You sure about this?')) {
+//             // this.todos = this.todoService.index();
+//             // }
+//             this.soundboardService.destroy(id).subscribe(
+//               data =>{
+//                 this.reload();
+//               },
+//               err => {
+//                 console.log('Error deleting soundboard: ' + err);
+//               }
+//             );
+//         }
+//         deleteExpression(id: number): void {
+//           // if (confirm('You sure about this?')) {
+//             // this.todos = this.todoService.index();
+//             // }
+//             this.expressionService.destroy(id).subscribe(
+//               data =>{
+//                 this.reload();
+//               },
+//               err => {
+//                 console.log('Error deleting expression: ' + err);
+//               }
+//             );
+//         }
       }
 
 
