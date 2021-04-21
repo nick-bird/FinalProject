afbc8c7092f056dde2c7e3ffe6f6775c27a13e46
Justin <>
2021-04-21 13:53:42 -0400

    built image class

--- ngEmmy/src/app/models/expression.ts
+++ ngEmmy/src/app/models/expression.ts
@@ -1,2 +1,17 @@
+import { Image } from "./image";
+import { User } from "./user";
+
 export class Expression {
+  id: number;
+  name: string;
+  pharse: string;
+  image: Image;
+  user: User;
+
+
+  constructor(id: number,name: string, pharse: string, image: string, user: User){
+      this.id = id;
+      this.name = name;
+      this.pharse = pharse;
+  }
 }
