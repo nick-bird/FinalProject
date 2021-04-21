74f6f74fd657d51276cf95283db85b9757690825
Justin <>
2021-04-21 13:28:05 -0400

    stubbed models and built user class

---
+++ ngEmmy/src/app/models/user.ts
@@ -0,0 +1,25 @@
+export class User {
+
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+  isRestricted: boolean;
+  isActive: boolean;
+
+  constructor(id?: number,firstName?: string,lastName?: string,username?: string, password?: string, email?: string,role?: string, isRestricted?: boolean, isActive?: boolean){
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.role = role;
+    this.isRestricted = isRestricted;
+    this.isActive = isActive;
+  }
+
+}
