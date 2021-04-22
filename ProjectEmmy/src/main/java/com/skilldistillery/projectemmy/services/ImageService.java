968f9f543e458aee2763ffa5036ba74ddd11a95d
Nick Bird <bird.nick.95@gmail.com>
2021-04-21 18:09:45 -0600

    finished image controller and service in Spring project

--- ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/services/ImageService.java
+++ ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/services/ImageService.java
@@ -6,4 +6,7 @@ import com.skilldistillery.projectemmy.entities.Image;
 
 public interface ImageService {
 List<Image> index();
+Image create(Image newImage);
+boolean destroy(int imageId);
+Image getById(int imageId);
 }
--- ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/services/ImageServiceImpl.java
+++ ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/services/ImageServiceImpl.java
@@ -7,15 +7,49 @@ import org.springframework.stereotype.Service;
 
 import com.skilldistillery.projectemmy.entities.Image;
 import com.skilldistillery.projectemmy.repositories.ImageRepository;
+import com.skilldistillery.projectemmy.repositories.UserRepository;
 @Service
 public class ImageServiceImpl implements ImageService {
 	
 	@Autowired
 	private ImageRepository imageRepo;
 	
+	
 	@Override
 	public List<Image> index() {
 	return imageRepo.findAll();
+	}
+
+	@Override
+	public Image create(Image newImage) {
+	return imageRepo.saveAndFlush(newImage);
+		
+	}
+
+	@Override
+	public boolean destroy(int imageId) {
+		
+		if (imageRepo.existsById(imageId)) {
+		imageRepo.deleteById(imageId);
+		
+		if(imageRepo.existsById(imageId)) {
+			return false;
+		}
+		
+		return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Image getById(int imageId) {
+		Image image = null;
+		try {
+			image = imageRepo.getOne(imageId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return image;
 	}
 
 }
