dee8f516a67d40e34c18f4c1878f7ae385af2a12
Nick Bird <bird.nick.95@gmail.com>
2021-04-25 12:54:01 -0600

    added ability to deactivate user in Spring project

--- ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/controllers/AuthController.java
+++ ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/controllers/AuthController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,4 +38,23 @@ public class AuthController {
 	public Principal authenticate(Principal principal) {
 	    return principal;
 	}
+	
+	
+	@PutMapping("/deactivate")
+	public User deactivate(Principal principal) {
+		User managedUser = authService.getUser(principal.getName());
+		authService.deactivate(managedUser);
+		return managedUser;
+		
+	}
+	
+	
+// FIXME?	
+//	@PutMapping("/reactivate")
+//	public User reactivate(Principal principal) {
+//		User managedUser = authService.getUser(principal.getName());
+//		authService.reactivate(managedUser);
+//		return managedUser;
+//		
+//	}
 }
--- ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/services/AuthService.java
+++ ProjectEmmy/src/main/java/com/skilldistillery/projectemmy/services/AuthService.java
@@ -5,4 +5,10 @@ import com.skilldistillery.projectemmy.entities.User;
 public interface AuthService {
 	
 	public User register(User user);
+	
+	public User deactivate(User user);
+	
+	public User reactivate(User user);
+	
+	public User getUser(String username);
 }
